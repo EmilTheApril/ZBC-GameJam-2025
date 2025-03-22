bfe0ded7c9ab9d4e7088e4d66b3a2ca88854afe8
EmilTheApril <pjuske.2002@hotmail.com>
2025-03-22 11:06:36 +0100

    Upgrade

--- Assets/_Scripts/Shooting.cs
+++ Assets/_Scripts/Shooting.cs
@@ -1,51 +1,81 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Shooting : MonoBehaviour
 {
-    private Camera mainCam;
     private Vector3 mousePos;
+    private Vector2 shootDir;
+    private float shootRot;
     public float KnockbackForce;
+    public float velocityBreakForce;
+    public float maxVelocity;
+    public float slowdownSpeed;
 
     public GameObject bullet;
     public Transform bulletTransform;
-    private Rigidbody2D PlayerRb;
+    public SpriteRenderer gunSpriteRenderer;
+    private Rigidbody2D rb;
 
-    public bool Canfire;
-    private float timer;
+    public bool canFire = true;
+    private bool shootButtonHeld = false;
     public float timeBetweenShots;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        PlayerRb = transform.parent.GetComponent<Rigidbody2D>();
-        mainCam = GameObject.Find("Main Camera").GetComponent<Camera>();
+        rb = transform.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 rotation = mousePos - transform.position;
-        float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, rotZ);
-        if (!Canfire)
+        SetRotation();
+        Shoot();
+        DampenSpeed();
+    }
+
+    public void SetRotation()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        shootDir = mousePos - transform.position;
+        shootRot = Mathf.Atan2(shootDir.y, shootDir.x) * Mathf.Rad2Deg;
+        transform.GetChild(0).rotation = Quaternion.Euler(0f, 0f, shootRot);
+
+        gunSpriteRenderer.flipY = transform.GetChild(0).rotation.z <= 90 && transform.GetChild(0).rotation.z >= -90 ? false : true;
+    }
+
+    public void OnFire(InputValue value)
+    {
+        shootButtonHeld = value.Get<float>() == 1 ? true : false;
+    }
+
+    public void ShootCooldown()
+    {
+        if (canFire) return;
+        canFire = true;
+    }
+
+    public void DampenSpeed()
+    {
+        if (Mathf.Abs(rb.linearVelocity.magnitude) <= maxVelocity && rb.linearVelocityY <= 0 && Mathf.Abs(rb.linearVelocityX) < 3.5f)
         {
-           timer += Time.deltaTime;
-           if (timer >= timeBetweenShots)
-           {
-               Canfire = true;
-               timer = 0;
-           }
+            rb.linearDamping = 1f;
+            return;
         }
-        if(Input.GetMouseButtonDown(0) && Canfire)
+        rb.linearDamping = Mathf.Lerp(Mathf.Abs(rb.linearVelocity.magnitude) * velocityBreakForce, maxVelocity, slowdownSpeed);
+    }
+
+    public void Shoot()
+    {
+        if (!canFire || !shootButtonHeld) return;
+        canFire = false;
+        for (int i = 0; i <= 5; i++)
         {
-            Canfire = false;
-            for (int i = 0; i <= 5; i++)
-            {
-                GameObject bulletsOBJ = Instantiate(bullet, bulletTransform.position, Quaternion.identity);
-                bulletsOBJ.transform.rotation = Quaternion.Euler(0f, 0f, rotZ + Random.Range(-5, 5));
-            }
-            PlayerRb.AddForce(-rotation * KnockbackForce * Time.fixedDeltaTime, ForceMode2D.Impulse);
+            GameObject bulletsOBJ = Instantiate(bullet, bulletTransform.position, Quaternion.identity);
+            bulletsOBJ.transform.rotation = Quaternion.Euler(0f, 0f, shootRot + Random.Range(-5, 5));
         }
+        rb.linearVelocityY = 0;
+        rb.AddForce(-shootDir.normalized * KnockbackForce, ForceMode2D.Impulse);
+        Invoke(nameof(ShootCooldown), timeBetweenShots);
     }
 }
