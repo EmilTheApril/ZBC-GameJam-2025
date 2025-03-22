9b5dddf2c43367f3171a1b91638a912b02bc7851
EmilTheApril <pjuske.2002@hotmail.com>
2025-03-22 11:43:58 +0100

    Cleaned up shootscript and updated scene and player

--- Assets/_Scripts/Shooting.cs
+++ Assets/_Scripts/Shooting.cs
@@ -11,6 +11,7 @@ public class Shooting : MonoBehaviour
     public float velocityBreakForce;
     public float maxVelocity;
     public float slowdownSpeed;
+    public Vector2 bulletSpreadAngle;
 
     public GameObject bullet;
     public Transform bulletTransform;
@@ -41,7 +42,8 @@ public class Shooting : MonoBehaviour
         shootRot = Mathf.Atan2(shootDir.y, shootDir.x) * Mathf.Rad2Deg;
         transform.GetChild(0).rotation = Quaternion.Euler(0f, 0f, shootRot);
 
-        gunSpriteRenderer.flipY = transform.GetChild(0).rotation.z <= 90 && transform.GetChild(0).rotation.z >= -90 ? false : true;
+        float zRot = transform.GetChild(0).eulerAngles.z;
+        gunSpriteRenderer.flipY = zRot > 90 && zRot < 270 ? true : false;
     }
 
     public void OnFire(InputValue value)
@@ -72,7 +74,7 @@ public class Shooting : MonoBehaviour
         for (int i = 0; i <= 5; i++)
         {
             GameObject bulletsOBJ = Instantiate(bullet, bulletTransform.position, Quaternion.identity);
-            bulletsOBJ.transform.rotation = Quaternion.Euler(0f, 0f, shootRot + Random.Range(-5, 5));
+            bulletsOBJ.transform.rotation = Quaternion.Euler(0f, 0f, shootRot + Random.Range(bulletSpreadAngle.x, bulletSpreadAngle.y));
         }
         rb.linearVelocityY = 0;
         rb.AddForce(-shootDir.normalized * KnockbackForce, ForceMode2D.Impulse);
